7d946427c354c81491968293b8899fa6e0236ff6
Anthony Morima jr <95931014+AnthonyBeerus@users.noreply.github.com>
2024-03-03 20:30:10 +0200

    feat: database for expenses

---
+++ lib/database/expense_database.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:project_mercury/models/expense.dart';
+
+class ExpenseDatabse extends ChangeNotifier {
+  static late Isar isar;
+  List<Expense> _allExpenses = [];
+
+  /*
+
+``S E T U P 
+
+  */
+
+  //*initialize db
+
+  /*
+
+  G E T T E R S
+
+  */
+
+
+  /*
+
+  O P E R A T I O N S
+
+  */
+
+  /*
+
+  H E L P E R   F U N C T I O N S
+
+  */
+}
